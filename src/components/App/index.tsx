2ed4f1292058cf627ceccb0cdfa5cf73c63b8abc
杨钊 <yangzhao03@kuaishou.com>
2021-03-23 16:01:59 +0800

    🛫️ deleteBy

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,10 +1,29 @@
-import React, { PropsWithChildren } from 'react';
+import React, { PropsWithChildren, useEffect } from 'react';
+import useSchema, { ActionEnum } from '../../hooks/use-schema';
+import keyCode from '../../common/key-code';
 import config from './config';
 import manifest from './manifest';
 
 import './index.scss';
 
 const App = ({ children }: PropsWithChildren<void>) => {
+    const [store, dispatch] = useSchema();
+    const { activityId, schema } = store;
+
+    const handleKeyPress = (event: KeyboardEvent) => {
+        if (event.code === keyCode.DELETE && activityId) {
+            dispatch(ActionEnum.DELETE, schema[activityId]);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('keydown', handleKeyPress);
+
+        return () => {
+            document.removeEventListener('keydown', handleKeyPress);
+        };
+    }, [activityId]);
+
     return <div className="jigsaw-app">{children}</div>;
 };
 
